3a963c7d9af290555abe770a4620be2ecb970f54
a-mrv <aydoganmrv@gmail.com>
2024-06-06 13:42:05 +0300

    [Feat] Add SalesApp

---
+++ SalesApp/SalesApp.Domain/Services/CarService.cs
@@ -0,0 +1,59 @@
+﻿using SalesApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesApp.Domain.Services
+{
+    public class CarService
+    {
+        private static List<Car> cars = new();
+
+        public void Add(Car car)
+        {
+            cars.Add(car);
+        }
+        public void PrintConsole() 
+        {
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"Markası : {car.Make}, Modeli : {car.Model}");
+            }
+            
+        }
+
+        public List<Car> GetAll()
+        {
+            return cars;
+        }
+        public void Remove(Guid Id)
+        {
+          foreach (var car in cars)
+            {
+                if (car.Id == Id)
+                {
+                    cars.Remove(car);
+                    break;
+                }
+
+            }
+            
+        }
+        public void UpDate(Guid Id,string Model)
+        {  
+            foreach (var car in cars)
+            {
+                if (car.Id == Id)
+                {
+                    car.Make= Model;
+                    break;
+                }
+            }
+        }
+
+    }
+    
+}
---
+++ SalesApp/SalesApp.WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SalesApp.Domain.Entities;
+using SalesApp.Domain.Services;
+
+namespace SalesApp.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        CarService carService= new CarService();
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Car car)
+        {   car.Id = Guid.NewGuid();
+            carService.Add(car);
+            return Ok();
+        }
+        [HttpGet("[action]")]
+        public IActionResult PrintToConsole() 
+        {
+            carService.PrintConsole();
+            return Ok(); 
+        }
+
+        [HttpGet("[action]")]
+
+        public IActionResult GetAll()
+        {
+            var cars = carService.GetAll();
+            return Ok(cars);
+        }
+
+        [HttpDelete]
+        public IActionResult Remove(Guid Id)
+        {
+            carService.Remove(Id);
+            return Ok();
+
+        }
+
+        [HttpPost]
+        public IActionResult UpDate(Guid Id,string Model)
+        {
+            carService.UpDate(Id,Model);
+            return Ok();
+        }
+    }
+}
