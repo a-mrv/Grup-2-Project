7b2650bbc53c526742e7f272a6039f2bc2484615
a-mrv <aydoganmrv@gmail.com>
2024-06-07 12:47:57 +0300

    add

--- SwitchExamples/SwitchExamples/Program.cs
+++ SwitchExamples/SwitchExamples/Program.cs
@@ -31,29 +31,47 @@
 //        break ;
 //}
 
-using SwitchExamples;
-using SwitchExamples.Entities;
-using SwitchExamples.Enums;
-using System.Data;
+//using SwitchExamples;
+//using SwitchExamples.Entities;
+//using SwitchExamples.Enums;
+//using System.Data;
 
-Product product = new()
+//Product product = new()
+//{
+//    Name = "Laptop",
+//    Price = 5000,
+//    State= ProductState.Discontinued,
+
+//};
+
+//switch (product.State)
+//{
+//    case ProductState.InStock:
+//        Console.WriteLine("Ürün stokta.");
+//        break;
+//    case ProductState.OutOfStock:
+//        Console.WriteLine("Ürün stokta değil.");
+//        break;
+//    case ProductState.Discontinued:
+//        Console.WriteLine("Ürün satıştan kaldırıldı.");
+//        break;
+//}
+
+List<int> temperatures = new List<int>() { -5, 0, 10, 15, 25, 35 };
+
+foreach (var temperature in temperatures)
 {
-    Name = "Laptop",
-    Price = 5000,
-    State= ProductState.Discontinued,
+    if (temperature <-10)
+        Console.WriteLine("İnanılmaz soğuk bir hava var.");
+    else if (temperature < 0)
+        Console.WriteLine("Çok soğuk bir hava var.");
+    else if (temperature < 10)
+        Console.WriteLine("Soğuk bir hava var.");
+    else if (temperature < 20)
+        Console.WriteLine("Ilık  bir hava var.");
+    else if (temperature < 30)
+        Console.WriteLine("Sıcak bir hava var.");
+    else
+        Console.WriteLine("Çok sıcak bir hava var.");
 
-};
-
-switch (product.State)
-{
-    case ProductState.InStock:
-        Console.WriteLine("Ürün stokta.");
-        break;
-    case ProductState.OutOfStock:
-        Console.WriteLine("Ürün stokta değil.");
-        break;
-    case ProductState.Discontinued:
-        Console.WriteLine("Ürün satıştan kaldırıldı.");
-        break;
 }
-
--- SwitchExamples/SwitchExamples/obj/Debug/net8.0/SwitchExamples.AssemblyInfo.cs
+++ SwitchExamples/SwitchExamples/obj/Debug/net8.0/SwitchExamples.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SwitchExamples")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a963c7d9af290555abe770a4620be2ecb970f54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6993a8e8b891b26b7c2f4885ea18f4005953cc33")]
 [assembly: System.Reflection.AssemblyProductAttribute("SwitchExamples")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SwitchExamples")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
