4bcc11eb92cd739260a97ff78400081848dc83c6
a-mrv <aydoganmrv@gmail.com>
2024-06-10 10:47:05 +0300

    [Feat] Add NoteExampleApp

---
+++ NotExampleApp/NotExampleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿List<string> nots = new();
+
+Console.WriteLine("NOT YAZMA SİSTEMİ");
+
+while (true)
+{
+    Console.WriteLine("1. Not Kaydet");
+    Console.WriteLine("2. Not Oku");
+    Console.WriteLine("3. Çıkış");
+    Console.Write("Seçiminiz: ");
+    string choice = Console.ReadLine();
+
+
+    switch (choice)
+    {
+        case "1":
+            Console.Write("Kaydedilecek not: ");
+            string not = Console.ReadLine();
+            Console.WriteLine(not);
+            break;
+        case "2":
+
+            Console.WriteLine(nots);
+            break;
+        case "3":
+            return;
+        default:
+            Console.WriteLine("Geçersiz seçim!");
+            break;
+    }
+}
