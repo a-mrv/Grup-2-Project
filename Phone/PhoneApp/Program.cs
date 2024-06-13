0588ceecc6ef3c70411f5c169f1b0f36008141f0
a-mrv <aydoganmrv@gmail.com>
2024-06-13 23:57:40 +0300

    Add

--- Phone/PhoneApp/Program.cs
+++ Phone/PhoneApp/Program.cs
@@ -9,6 +9,22 @@ PhoneService phoneService = new();
 özellikler.Açıklama = "15 Pro Max";
 özellikler.HatSayısı = "Çift hatlı";
 
+Batarya batarya = new();
+batarya.Kapasitesi = 5;
+batarya.Şarj ="";
+batarya.Teknolojisi = "";
+
+Ekran ekran = new();
+ekran.Boyutu =12;
+ekran.Çözünürlügü =128;
+ekran.YenilemeHızı = 25;
+
+Kamera kamera= new();
+kamera.ArkaKameraÇözünürlük ="";
+kamera.Çözünürlük ="";
+kamera.OdakUzaklıgı =25;
+
+Console.WriteLine
 
 
 
