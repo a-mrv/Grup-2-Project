33c8c90a5d8d99057f8fd4747a3831f97db3036e
a-mrv <aydoganmrv@gmail.com>
2024-06-14 14:52:25 +0300

    Add

--- ExampleProject/ExampleProject.Domain/Services/TurService.cs
+++ ExampleProject/ExampleProject.Domain/Services/TurService.cs
@@ -3,6 +3,7 @@ using ExampleProject.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,13 +13,55 @@ namespace ExampleProject.Domain.Services
     {
         public void Add(List<string> şehirler, List<string> katılımcılar, DateTime tarih, double ücret)
         {
-            string text = $"{şehirler},{katılımcılar},{tarih},{ücret}\n";
-            File.AppendAllText("C:\\Users\\MERVE\\Documents\\GitHub\\Grup-2-Project\\ExampleProject\\ExampleProject.Domain\\Data.txt",text);
+            string şehirText = "";
+            foreach (var şehir in şehirler)
+            {
+                şehirText += $"{şehir}-";
+            }
+
+            string katılımcıText = "";
+            foreach (var katılımcı in katılımcılar)
+            {
+                katılımcıText += $"{katılımcı}-";
+            }
+            string text = $"{şehirText},{katılımcıText},{tarih},{ücret}\n";
+            File.AppendAllText("C:\\Users\\MERVE\\Documents\\GitHub\\Grup-2-Project\\ExampleProject\\ExampleProject.Domain\\Data.txt", text);
         }
 
         public List<Tur> GetAll()
         {
-            throw new NotImplementedException();
+            string text = File.ReadAllText("C:\\Users\\MERVE\\Documents\\GitHub\\Grup-2-Project\\ExampleProject\\ExampleProject.Domain\\Data.txt");
+            var turs = new List<Tur>();
+            string[] lines = text.Split('\n');
+
+            foreach (var line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    continue;
+                }
+                string[] columns = line.Split(",");
+
+                string[] şehirler = columns[0].Split('-');
+                string[] katılımcılar = columns[1].Split('-');
+
+                //string şehirlerStr = string.Join(", ", şehirler);
+                //string katılımcılarStr = string.Join(", ", katılımcılar);
+
+                Tur tur = new Tur
+                {
+
+                    Şehirler = şehirler.ToList(),
+                    Katılımcılar = katılımcılar.ToList(),
+                    Tarih = DateTime.Parse(columns[2]),
+                    Ücret = double.Parse(columns[3]),
+                };
+
+                turs.Add(tur);
+
+            }
+            return turs;
+
         }
     }
 }
--- ExampleProject/TurApp/Program.cs
+++ ExampleProject/TurApp/Program.cs
@@ -11,13 +11,45 @@ List<string> şehirler = new List<string>();
 şehirler.Add("Rize");
 şehirler.Add("Artvin");
 
-var tur = new Tur
-{
-    Şehirler = şehirler
 
-};
 Console.WriteLine("Gezilecek Şehirler");
 foreach (var item in şehirler)
 {
     Console.WriteLine(item);
 }
+List<string> katılımcılar = new List<string>();
+katılımcılar.Add("Merve Aydoğan");
+katılımcılar.Add("Ayşe Ay");
+katılımcılar.Add("Halil Kaya");
+katılımcılar.Add("Ali Kaya");
+katılımcılar.Add("Elif Dal");
+
+Console.WriteLine("Katılımcılar");
+foreach (var item in katılımcılar)
+{
+    Console.WriteLine(item);
+}
+
+var tur = new Tur
+{
+    Şehirler = şehirler,
+    Katılımcılar = katılımcılar,
+    Tarih= DateTime.Now,
+    Ücret = 100,
+
+};
+
+turService.Add(tur.Şehirler, tur.Katılımcılar, tur.Tarih, tur.Ücret);
+
+List<Tur> turlar = turService.GetAll();
+
+string kayitDosyasi = "C:\\Users\\MERVE\\Documents\\GitHub\\Grup-2-Project\\ExampleProject\\ExampleProject.Domain\\Kayit.txt";
+
+foreach (var item in turlar)
+{
+    Console.WriteLine($"Şehirler: {tur.Şehirler}, Katılımcılar: {tur.Katılımcılar}, Tarih: {tur.Tarih}, Ücret: {tur.Ücret}");
+}
+
+Console.ReadLine();
+        
+
