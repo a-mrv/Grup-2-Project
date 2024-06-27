0c322e0226d47a2f502898ec30c789589916256f
a-mrv <aydoganmrv@gmail.com>
2024-06-27 15:56:19 +0300

    Add

--- ContactDbApp/ContactDbApp.Domain/obj/Debug/net8.0/ContactDbApp.Domain.AssemblyInfo.cs
+++ ContactDbApp/ContactDbApp.Domain/obj/Debug/net8.0/ContactDbApp.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactDbApp.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3272884df71655c2ba55d02513f66c5ba9daec77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac99aabaf64b147c187ec537ef8eed5243f2f442")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactDbApp.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactDbApp.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
