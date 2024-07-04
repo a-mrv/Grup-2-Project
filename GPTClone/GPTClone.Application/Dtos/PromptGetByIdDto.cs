063c97dbcf1e3888264e5ea9488f07a7b994cef7
a-mrv <aydoganmrv@gmail.com>
2024-07-04 18:20:17 +0300

    Add

---
+++ GPTClone/GPTClone.Application/Dtos/PromptGetByIdDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPTClone.Application.Dtos
+{
+  public class PromptGetByIdDto
+    {
+        public Guid Id { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
+        public string Text { get; set; }
+        public string Response { get; set; }
+    }
+}
