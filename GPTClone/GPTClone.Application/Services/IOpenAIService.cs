843b8dabc050c3c11d32fdba6f1b14032aa4a04f
a-mrv <aydoganmrv@gmail.com>
2024-07-04 18:20:46 +0300

    Add

---
+++ GPTClone/GPTClone.Application/Services/IOpenAIService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPTClone.Application.Services
+{
+    public interface IOpenAIService
+    {
+        string GetResponse(string text);
+    }
+}
