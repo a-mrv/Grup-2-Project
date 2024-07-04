6464570136249da5bfeab69ff80c4409593aa885
a-mrv <aydoganmrv@gmail.com>
2024-07-04 18:21:04 +0300

    Add

---
+++ GPTClone/GPTClone.Domain/Entities/Prompt.cs
@@ -0,0 +1,16 @@
+﻿using GPTClone.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPTClone.Domain.Entities
+{
+    public class Prompt: EntityBase
+    {
+       
+        public string Text { get; set; } // sorduğumuz soru
+        public string Response { get; set; } // aldığımız cevap
+    }
+}
